d50c997a9e61860a793e910b7f0dc046889b4dd7
Ryan Bauroth <ryanbauroth6@gmail.com>
2022-05-16 23:06:46 -0400

    works

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ const id = "764241702562037773";
 //const id = '975158615520460882'
 
 var zecQuotes = ['No bitches?','Did I ask?','Folded.','Simmer down lil bro','ratio',':skull:','bruh','are you freaking kidding me bro','SHELLY IS SO DUMB ITS UNFAIR','Ok but where is your rank 30 brawler?', 'so bad', 'so fat', ':smirk_cat::thumbsup:','HUUUUUUUUUUUUUUH?????','kid','this is brain damage','smooth brain',':moyai:','this is so aids','Common L', 'so good','ion wanna hear it','typical proccess','Process?',"Process check?",'aint no way','no shot','no freaking way dude','no shot','tf do you mean','mid','no shot bro','you fumbled the bag','why did you add this bot tho','u think ur some sort of comedian today huh?','Nah','this is degenerate','no rank 30']
-//				@Desl					2					@Josh				2					3						@Turtle						@P1				@Xtroyer			@f1				@ange					@froot				2						tyler				jman				pigs
-var pings = ['537805054334337033','537805054334337033','835574860772147250','835574860772147250','835574860772147250','479656417104494602','656232624309927956','498524408604786715','479645386751868938','758380474195378236','728771398561955912','728771398561955912','607588966392266756','468126093606387712','744683791443820678']
+//				@Desl					2					@Josh				2					3						@Turtle						@P1				@Xtroyer			@f1				@ange					@froot				2						tyler				jman				pigs '537805054334337033','537805054334337033','835574860772147250','835574860772147250','835574860772147250','479656417104494602','656232624309927956','498524408604786715','479645386751868938','758380474195378236','728771398561955912','728771398561955912','607588966392266756','468126093606387712','744683791443820678'
+var pings = ['537805054334337033']
 var muted = false;
 
 // When the client is ready, run this code (only once)
@@ -45,30 +45,45 @@ client.on('messageCreate', (message) => {
 	}
 	//stops @s
 	if(message.content === '!zecPersonalMute' || message.content === '!zecPM'){
+		var foundAuthor = false;
 		for (let x = 0; x < pings.length; x++)
 		{
-			if(message.author.user.id === pings[x])
+			if(message.author.id === pings[x])
 			{
-				//maybe jsut message.author
+				foundAuthor = true;
+				//removes user from list pings
 				pings.splice(x)
-				console.log(pings[pings.length]);
+				//verifies this happens to user
+				message.reply({
+					content: 'Removed user ' + message.author.id + ' or ' + message.author.username + ' from pings list'
+				})
+				//lets me know this happened in case I need to restart and should update list
+				console.log('Removed user ' + pings[pings.length - 1] + ' or ' + message.author.username + ' from pings list')
 			}
+		}
+		if(foundAuthor === false){
+			message.reply('Looks like you are already not reciving pings. Whoops!');
 		}
 	}
 	//starts @s
 	if(message.content === '!zecPersonalUnmute' || message.content === '!zecPUM'){
 		var foundAuthor = false;
 		for (let x = 0; x < pings.length; x++){
-			if(message.author.user.id === pings[x]){
-				//maybe just message.author
+			if(message.author.id === pings[x]){
 				foundAuthor = true;
+				message.reply('Looks like you are already reciving pings silly!')
 			}
 		}
 		if (foundAuthor === false)
 		{
-			pings.push(message.author.user.id);
-			//same thing
-			console.log(pings[pings.length]);
+			//adds user to list pings
+			pings.push(message.author.id);
+			//verfies this happens to user
+			message.reply({
+				content: 'Added user ' + message.author.id + ' or ' + message.author.username + ' to ping list'
+			})
+			//lets me know this happened in case I need to restart and should update list
+			console.log('Added user ' + pings[pings.length - 1] + ' or ' + message.author.username + ' to ping list');
 		}
 	}
 	//suggests phrases
@@ -82,15 +97,16 @@ client.on('messageCreate', (message) => {
 			message.reply({
 				content:zecQuotes[randomQuote],
 			})
+			//message.react(':skull:')
 			if (randomQuote === 4){
 				//if its "ratio" 
-				message.react(':thumbsup:')
+				//message.react(':thumbsup:')
 			}
 		}
 		// dont reply to urself
 		else if(message.author.bot) return;
 		//random chance to reply
-		else if(getRandomInt(10) === 1){
+		else if(getRandomInt(50) === 1){
 			//chooses whether to reply or randomly ping people
 			if(getRandomInt(7) === 1){
 				var channel = client.channels.fetch(id).then(channel => channel.send(`<@${pings[getRandomInt(pings.length)]}>` + ' ' + zecQuotes[getRandomInt(zecQuotes.length)]))
